a111dce19e435aa20f8ddce2d418eae460fe3c7e
Theo Baptista <theoalfonso78@gmail.com>
2021-04-15 17:25:03 -0300

    Corrigindo mais linhas do código

--- src/main/java/br/com/zupacademy/proposta/proposta/CriaPropostaController.java
+++ src/main/java/br/com/zupacademy/proposta/proposta/CriaPropostaController.java
@@ -8,15 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.net.URI;
 
 @RestController
 @RequestMapping("api/propostas")
@@ -32,6 +31,7 @@ public class CriaPropostaController {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<?> criaProposta(@Valid @RequestBody PropostaRequest propostaRequest, UriComponentsBuilder builder) {
 
         if (repository.existsByDocumento(propostaRequest.getDocumento())) {
@@ -40,20 +40,19 @@ public class CriaPropostaController {
         }
 
         Proposta proposta = propostaRequest.toModel();
-        repository.save(proposta);
-        logger.info("Proposta documento={} salário={} criada com sucesso!", proposta.getDocumento(), proposta.getSalario());
+
+
 
         Boolean solicitanteNaoTemRestricao = verificarRestricao(new ConsultaRestricaoSolicitanteRequest(proposta));
         proposta.validarRestricao(solicitanteNaoTemRestricao);
 
 
         repository.save(proposta);
-        logger.info("Status da análise financeira atualizado com sucesso! Id da proposta: {} Status:{}", proposta.getId(), proposta.getStatus());
+        logger.info("Proposta criada com sucesso! documento={} salário={} status da proposta={}", proposta.getDocumento(), proposta.getSalario(),proposta.getStatus());
 
-        URI uri = builder.path("/api/propostas/{id}")
-                .buildAndExpand(proposta.getId()).toUri();
 
-        return ResponseEntity.created(uri).build();
+
+        return ResponseEntity.created(builder.path("/api/propostas/{id}").build(proposta.getId())).build();
     }
 
     private Boolean verificarRestricao(ConsultaRestricaoSolicitanteRequest request) {
@@ -62,12 +61,6 @@ public class CriaPropostaController {
             return true;
 
         } catch (FeignException.UnprocessableEntity e) {
-            return false;
-
-        } catch (Exception e) {
-
-            logger.error("Erro ao processar a requisição http para solicitar a consulta financeira!!", e);
-            Assert.state(false, "Erro ao processar a requisição http para solicitar a consulta financeira!!");
             return false;
         }
     }
