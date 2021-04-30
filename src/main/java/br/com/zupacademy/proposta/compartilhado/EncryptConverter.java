0bb9b7e5884f72c17f0acd04e9eec8a4aefc28a4
Theo Baptista <theoalfonso78@gmail.com>
2021-04-30 18:08:52 -0300

    Refatorando o salt da classe converter

--- src/main/java/br/com/zupacademy/proposta/compartilhado/EncryptConverter.java
+++ src/main/java/br/com/zupacademy/proposta/compartilhado/EncryptConverter.java
@@ -1,6 +1,7 @@
 package br.com.zupacademy.proposta.compartilhado;
 
 import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 
 import javax.persistence.AttributeConverter;
 
@@ -8,15 +9,15 @@ public class EncryptConverter implements AttributeConverter<String,String> {
 
 
     private final String password = "password";
-    private final String salt = "5c0744940b5c369b";
+    private final String salt = KeyGenerators.string().generateKey();
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
-        return Encryptors.queryableText(this.password, this.salt).encrypt(attribute);
+        return Encryptors.text(this.password, this.salt).encrypt(attribute);
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
-        return Encryptors.queryableText(this.password,this.salt).decrypt(dbData);
+        return Encryptors.text(this.password,this.salt).decrypt(dbData);
     }
 }
